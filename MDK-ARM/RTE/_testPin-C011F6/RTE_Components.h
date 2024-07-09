0e8afec4cb477f81c8cef0e4ec400d8e4791c942
Roberta Diaferia <roberta.diaferia@gmmspa.com>
2024-07-09 09:25:11 +0200

    Add uvprojx files

---
+++ MDK-ARM/RTE/_testPin-C011F6/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'testPin-C011F6' 
+ * Target:  'testPin-C011F6' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32c0xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
