e7a633e2659ba262cfab48e0a78776cc588203a0
Roberta Diaferia <roberta.diaferia@gmmspa.com>
2024-07-09 09:24:13 +0200

    Add Keil proj files

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -74,6 +74,7 @@ static void MX_I2C1_Init(void);
 int main(void)
 {
   /* USER CODE BEGIN 1 */
+	//prova2
 
   /* USER CODE END 1 */
 
@@ -108,6 +109,7 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
+	  // prova
 
     /* USER CODE BEGIN 3 */
   }
